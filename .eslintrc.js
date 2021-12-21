117be009d30484bca8870120c5ed37995414b079
SteveReid <steve@appby.design>
2021-12-21 14:37:53 +0000

    install and configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,9 @@ module.exports = {
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
